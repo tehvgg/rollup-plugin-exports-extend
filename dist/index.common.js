e76f68646629f4fbc8502b3b93d88b6def733279
Patrick McGuckin <p.mcgooks@gmail.com>
2017-12-15 14:58:24 -0700

    Pass a null map so rollup ignores the map. Requires custom rollup.

--- dist/index.common.js
+++ dist/index.common.js
@@ -1,22 +1,22 @@
 'use strict';
 
 function exportsExtend(renamedObject) {
-  var exportRegex = /}\({}(.*)(\))?\);$/;
-  var exportReplace = '}(' + renamedObject + '$1$2);';
-  return {
-    name: 'exports-extend',
-    transformBundle: function transformBundle(source, _ref) {
-      var format = _ref.format;
+	var exportRegex = /}\({}(.*)(\))?\);$/;
+	var exportReplace = '}(' + renamedObject + '$1$2);';
+	return {
+		name: 'exports-extend',
+		transformBundle: function transformBundle(source, _ref) {
+			var format = _ref.format;
 
-      if (format !== 'iife') {
-        throw new Error('This plugin only works for IIFE bundles. Format given: ' + format);
-      }
-      return {
-        code: source.replace(exportRegex, exportReplace),
-        map: { mappings: '' }
-      };
-    }
-  };
+			if (format !== 'iife') {
+				throw new Error('This plugin only works for IIFE bundles. Format given: ' + format);
+			}
+			return {
+				code: source.replace(exportRegex, exportReplace),
+				map: null
+			};
+		}
+	};
 }
 
 module.exports = exportsExtend;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 export default function exportsExtend (renamedObject) {
-  const exportRegex = /}\({}(.*)(\))?\);$/
-  const exportReplace = `}(${renamedObject}$1$2);`;
-  return {
-    name: 'exports-extend',
-    transformBundle (source, { format }) {
-      if (format !== 'iife') {
-        throw new Error(`This plugin only works for IIFE bundles. Format given: ${format}`);
-      }
-      return {
-        code: source.replace(exportRegex, exportReplace),
-        map: { mappings: '' }
-      };
-    }
-  };
+	const exportRegex = /}\({}(.*)(\))?\);$/
+	const exportReplace = `}(${renamedObject}$1$2);`;
+	return {
+		name: 'exports-extend',
+		transformBundle (source, { format }) {
+			if (format !== 'iife') {
+				throw new Error(`This plugin only works for IIFE bundles. Format given: ${format}`);
+			}
+			return {
+				code: source.replace(exportRegex, exportReplace),
+				map: null
+			};
+		}
+	};
 }
